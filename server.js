07aeb2af764fb4ab62af278b8ee9a13870565cc5
Luke Rossi <luke.rossi@dealer.com>
2017-02-06 18:50:08 +0000

    commiting server.js

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
-var express = require('express')
-var app = express()
+// enable 'strict mode'
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// required packages
+var express = require('express'),
+    routes = require('./app/routing/index.js'),
+    bodyParser = require('body-parser'), // call 'body-parser' middleware to extract the entire body portion of an incoming request stream and expose it on req.body as something easier to interface with
+    api = require('./app/api/timestamp.js'),
+    app = express();
+    
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.use(function(req, res, next) {
+    res.header("Acess-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Method", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+routes(app);
+api(app);
+
+app.set('port', (process.env.PORT || 8080)); // define port
+app.listen(app.get('port'), function() {
+    console.log('Listening on port ' + app.get('port')); //log the port in console
+});
+
